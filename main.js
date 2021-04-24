e2220379d6c4cb84c83069504d0fbe81c20e3fa4
AsheeshMishra <69682206+AsheeshMishra@users.noreply.github.com>
2021-04-24 16:52:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function openForm(){
+    document.getElementById('container-box-form').style.display="block";
+    document.getElementById('open-button').style.display="none";
+   }
+
+   function closeForm(){
+    document.getElementById('container-box-form').style.display="none";
+    document.getElementById('open-button').style.display="block";
+   }
